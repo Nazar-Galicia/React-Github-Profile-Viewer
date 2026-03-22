d2f79eb171a8b8ac20c7f5b66b7df99a55d301fc
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-03-22 11:53:44 +0200

    style: add gaps for heading and subtitle

--- src/components/Viewer/Viewer.jsx
+++ src/components/Viewer/Viewer.jsx
@@ -3,8 +3,10 @@ import "./Viewer.css"
 const Viewer = () => {
     return (
         <div className="viewer">
-            <h1 className="viewer__heading">GitHub Users</h1>
-            <p className="viewer__subtitle">Explore and search for github users</p>
+            <div className="viewer__text-wrapper">
+                <h1 className="viewer__heading">GitHub Users</h1>
+                <p className="viewer__subtitle">Explore and search for github users</p>
+            </div>
         </div>
     )
 }
