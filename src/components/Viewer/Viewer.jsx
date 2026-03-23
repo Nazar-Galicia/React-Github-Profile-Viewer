c31f92d95eebea34c16caea15811d2bccc212eb9
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-03-23 16:22:34 +0200

    feat: add infinity scroll

--- src/components/Viewer/Viewer.jsx
+++ src/components/Viewer/Viewer.jsx
@@ -1,7 +1,49 @@
 import UsersList from "@/components/UsersList/UsersList.jsx";
+import {useEffect, useRef, useState} from "react";
+import githubAPI from "@/api/githubAPI";
 import "./Viewer.css"
 
 const Viewer = () => {
+    const observerRef = useRef(null);
+    const [page, setPage] = useState(0);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        setPage((prevState) => prevState + 1);
+                    }
+                });
+            },
+            {
+                threshold: 0.1,
+            }
+        );
+
+        if (observerRef.current) {
+            observer.observe(observerRef.current);
+        }
+
+        return () => {
+            if (observerRef.current) observer.unobserve(observerRef.current);
+        };
+    }, []);
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        console.log(page)
+        githubAPI.getUsers(page).then((users) => {
+            setUsers(prev => {
+                const merged = [...prev, ...users];
+
+                return [...new Map(merged.map(u => [u.id, u])).values()];
+            });
+            console.log(users)
+        })
+    }, [page])
+
     return (
         <div className="viewer">
             <div className="container">
@@ -9,8 +51,9 @@ const Viewer = () => {
                     <h1 className="viewer__heading">GitHub Users</h1>
                     <p className="viewer__subtitle">Explore and search for github users</p>
                 </div>
-                <UsersList />
+                <UsersList users={users} />
             </div>
+            <div ref={observerRef}></div>
         </div>
     )
 }
