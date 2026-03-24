847099f5dc51189a23e41e19bacd6bc9da526300
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-03-24 14:16:33 +0200

    feat: user page make request for searching user by id

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,6 +1,23 @@
+import {useParams} from "react-router-dom";
+import {useEffect, useRef} from "react";
+import githubAPI from "@/api/githubAPI.js";
+
 const User = () => {
+    const {
+        id,
+    } = useParams();
+
+    const userVar = useRef([])
+
+    useEffect(() => {
+        githubAPI.getUser(id).then((user) => {
+            userVar.current = user;
+            console.log(userVar.current);
+        })
+    }, [])
+
     return (
-        <h1>User page</h1>
+        <h1>User page: {userVar.current}</h1>
     )
 }
 
