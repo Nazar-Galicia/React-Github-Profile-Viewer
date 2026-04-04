f4c51c90b73f39d5b2036d940df4e5b5c1e51aae
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-04-04 14:22:44 +0300

    feat: create loader component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+const Loader = () => {
+    return (
+        <div className="loader">
+            <div className="loader__spinner"></div>
+
+            <p className="loader__text">
+                Loading GitHub data...
+            </p>
+        </div>
+    )
+}
+
+export default Loader;
