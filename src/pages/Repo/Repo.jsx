ec1af03cc2b64c3e31de904f6afca22d192f2739
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-04-08 18:32:29 +0300

    feat: add loader

--- src/pages/Repo/Repo.jsx
+++ src/pages/Repo/Repo.jsx
@@ -2,6 +2,7 @@ import Repository from "@/components/Repository/Repository.jsx";
 import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import githubApi from "@/api/githubAPI.js";
+import Loader from "@/components/Loader/Loader.jsx";
 
 const Repo = () => {
     const {
@@ -15,7 +16,7 @@ const Repo = () => {
         githubApi.getRepository(userId, repoId).then(setRepo);
     }, [repoId, userId]);
 
-    console.log();
+    if (!repo || !repo.owner) return <Loader>Loading GitHub data...</Loader>;
 
     return (
         <Repository repo={repo}/>
