e80c09ec75db158d0a8e806e78f03b0d7d5f5c0d
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-04-09 12:04:41 +0300

    feat: create list for commits and add styles

---
+++ src/components/RepositoryCommitsList/RepositoryCommitsList.jsx
@@ -0,0 +1,36 @@
+import CommitCard from "@/components/CommitCard/CommitCard.jsx";
+import './RepositoryCommitList.css'
+
+const RepositoryCommitsList = (props) => {
+    const {
+        commits,
+    } = props
+
+    return (
+        <div className="repo-commits">
+
+            <div className="repo-commits__header">
+                <h2 className="repo-commits__title">Commits</h2>
+                <span className="repo-commits__count">{commits.length}</span>
+            </div>
+
+            <div className="repo-commits__list">
+
+                {commits.map(commit => (
+                    <CommitCard
+                        key={commit.sha}
+                        commit={commit.commit}
+                        author={commit.commit.author}
+                        sha={commit.sha}
+                        avatarUrl={commit.author?.avatar_url}
+                        htmlUrl={commit.html_url}
+                    />
+                ))}
+
+            </div>
+
+        </div>
+    )
+}
+
+export default RepositoryCommitsList;
