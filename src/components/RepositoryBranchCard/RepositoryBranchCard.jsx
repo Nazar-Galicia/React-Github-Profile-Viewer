8ee4ea14e903ec35f9a47bccc1d5b249cdc655d2
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-04-13 12:30:31 +0300

    styles: add styles for barnches list and card component

--- src/components/RepositoryBranchCard/RepositoryBranchCard.jsx
+++ src/components/RepositoryBranchCard/RepositoryBranchCard.jsx
@@ -1,3 +1,5 @@
+import './RepositoryBranchCard.css'
+
 const RepositoryBranchCard = (props) => {
     const {
         name,
--- src/components/RepositoryBranchesList/RepositoryBranchesList.jsx
+++ src/components/RepositoryBranchesList/RepositoryBranchesList.jsx
@@ -1,6 +1,7 @@
 import {useContext} from "react";
 import {RepoContext} from "@/context/RepoContext.jsx";
 import RepositoryBranchCard from "@/components/RepositoryBranchCard/RepositoryBranchCard.jsx";
+import './RepositoryBranchesList.css'
 
 const RepoBranchesList = () => {
     const {
